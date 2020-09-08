b4d81cd757fa661ff579e7241f7406e1abc3b817
Remy2072 <30528@ma-web.nl>
2020-09-08 17:24:23 +0200

    gedicht

---
+++ PoemPrintingRemy.py
@@ -0,0 +1,17 @@
+zin1 = "ik ben Remy ik ben 17jaar"
+zin2 = "soms doe ik een beetje raar"
+zin3 = "ik hou van gamen jajaja"
+zin4 = "soms schiet ik in spelletjes rara"
+zin5 = "maar ik kan ook bouwen"
+zin6 = "soms kan best wel snauwen"
+zin7 = "maar ik ben geen ouwe"
+zin8 = "toch kan je mij vertrouwen"
+
+print(zin1)
+print(zin2)
+print(zin3)
+print(zin4)
+print(zin5)
+print(zin6)
+print(zin7)
+print(zin8)
