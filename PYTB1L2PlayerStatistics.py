c71e656ee13dccd662c1e3cdc51d5fd7993054df
Remy2072 <30528@ma-web.nl>
2020-09-09 12:18:02 +0200

    new file

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,22 @@
+zin1 = "name: Remy Duivesteijn"
+zin2 = "level: 82"
+zin3 = "Lives: 6"
+zin4 = "special power: flying"
+zin5 = "team: black"
+zin6 = "stamina: 78%"
+zin7 = "vehicle: spaceship"
+zin8 = "weapon: black orbs"
+zin9 = "ammo: 10"
+zin10 = "wanted: false"
+
+
+print(zin1)
+print(zin2)
+print(zin3)
+print(zin4)
+print(zin5)
+print(zin6)
+print(zin7)
+print(zin8)
+print(zin9)
+print(zin10)
