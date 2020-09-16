0391cdb842869afb90e95d403e6cfbf42c7e1178
Remy2072 <30528@ma-web.nl>
2020-09-16 12:49:29 +0200

    my first operation

---
+++ PYTB1L2MyFirstOperations.py
@@ -0,0 +1,40 @@
+# ADDITION
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The addition of those 2 numbers =", x + y, "\n")
+
+
+# SUPTRACTION
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The subtraction of those 2 numbers =", x - y, "\n")
+
+
+# MULTIPLICATION
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The multiplication of those 2 numbers =", x * y, "\n")
+
+
+# DIVISION
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The division of those 2 numbers =", x / y, "\n")
+
+
+# MODULO
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The modulo of those 2 numbers =", x % y, "\n")
+
+
+# ADDITION
+x+= int(input("x += y: "))
+print("The addition of those 2 numbers =", x,"\n")
+
+
+# SUPTRACTION
+x/= int(input("x /= y: "))
+print("The addition of those 2 numbers =", x)
+
+
