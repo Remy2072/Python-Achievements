0ec2af28e8335cade2da8a7adeac0323f82222dc
Remy2072 <30528@ma-web.nl>
2020-09-17 20:51:44 +0200

    2e versie verbeterd

--- PYTB1L2HowYouLikeThemApples.py
+++ PYTB1L2HowYouLikeThemApples.py
@@ -2,23 +2,33 @@ import math
 
 #Vervang de 0-en met de juiste berekeningen
 
-trees               = 333           #hoeveel bomen zijn er in totaal?
+trees               = 333            #hoeveel bomen zijn er in totaal?
 shadedTrees         = math.ceil(222) #hoeveel bomen staan er in de schaduw (afgerond naar boven)
-sunnyTrees          = 111           #hoeveel in de zon?
+sunnyTrees          = 111            #hoeveel in de zon?
 
-shadeOutputModifier = 80            #hoeveel procent productie geeft een schaduwboom?
+shadeOutputModifier = 80             #hoeveel procent productie geeft een schaduwboom?
+ 
+sunnyTreeOutput     = 146            #hoeveel appels geeft 1 zonnige boom?
+shadedTreeOutput    = 117            #hoeveel appels geeft 1 schaduw boom?
 
-sunnyTreeOutput     = 146           #hoeveel appels geeft 1 zonnige boom?
-shadedTreeOutput    = 116.8         #hoeveel appels geeft 1 schaduw boom?
+sunnyOutput         = 16206          #hoeveel appels geven alle zonnige bomen? 
+shadedOutput        = 25794          #hoeveel appels geven alle schaduw bomen?
+totalOutput         = 48438          #hoeveel appels zijn er in totaal?
 
-sunnyOutput         = 16.206        #hoeveel appels geven alle zonnige bomen? 
-shadedOutput        = 25929.6      #hoeveel appels geven alle schaduw bomen?
-totalOutput         = 42135.6      #hoeveel appels zijn er in totaal?
+owners              = 4              #met hoeveel mensen verdelen we de appels?
 
-owners              = 3             #met hoeveel mensen verdelen we de appels?
+eatCount            = 6438           #hoeveel appels houden we over omdat ze niet eerlijk te verdelen zijn?
+sellableOutput      = 42000          #hoeveel appels zijn er over en dus verkoopbaar?
+cut                 = 10500         #hoeveel appels mag ik verkopen?
 
-eatCount            = 0             #hoeveel appels houden we over omdat ze niet eerlijk te verdelen zijn?
-sellableOutput      = 14.045         #hoeveel appels zijn er over en dus verkoopbaar?
-cut                 = 14.045         #hoeveel appels mag ik verkopen?
 
-print(cut)
+print("Dit zijn de sunny trees\n",trees - shadedTrees)
+print("Dit zijn de shaded trees\n",trees - sunnyTrees)
+print("Zonnige appels in totaal\n",sunnyTrees * sunnyTreeOutput)
+print("Schadow appels in totaal\n",shadedTrees * shadedTreeOutput)
+print("Eetbare appels in totaal\n",shadedOutput + sunnyOutput + 29 * 222)
+print("Niet etbare appels\n", 29 * 222)
+print("aantal die je kan verkopen\n",totalOutput - eatCount)
+print("Delen met vrienden",sellableOutput / owners)
+print("alle appels in totaal\n",cut)
+
