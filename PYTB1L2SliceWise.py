9d980132974abafe871e4511a35369b0bdd47d95
Remy2072 <30528@ma-web.nl>
2020-09-30 12:42:46 +0200

    slice wise

---
+++ PYTB1L2SliceWise.py
@@ -0,0 +1,46 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+
+voornaam = tekst[37:41]
+print(voornaam)
+# Erik
+voornaam = tekst[43:48]
+print(voornaam)
+# Erwin
+voornaam = tekst[61:66]
+print(voornaam)
+# Hidde
+
+tekst = 'SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ...'
+
+vakken = tekst[15:21]
+print()
+print(vakken)
+# Python
+
+vakken = tekst[23:26]
+print(vakken)
+# UXD
+
+vakken = tekst[28:48]
+print(vakken)
+# Frontend development
+
+vakken = tekst[52:71]
+print(vakken)
+# Backend development
+
+tekst = 'Wat is hier het laatste woord?'
+
+letters = tekst[24:30]
+print()
+print(letters)
+# woord
+
+tekst = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+
+letter = tekst[5:8]
+print()
+print(letter)
+
+letter = tekst[29:32]
+print(letter)
