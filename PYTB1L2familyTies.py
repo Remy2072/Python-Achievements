77bcfa60415174f88f6346cd05b6c9ebb4799926
Remy2072 <30528@ma-web.nl>
2020-09-30 11:38:22 +0200

    familyties1

---
+++ PYTB1L2familyTies.py
@@ -0,0 +1,16 @@
+naam 		= "Remy"
+leeftijd	= "17"
+woonplaats	= "Amsterdam Noord"
+huisdier	= "Kat"
+werk		= "Albert Heijn"
+school		= "Mediacollege Amsterdam"
+opleiding	= "software developer"
+afstand		= "12.4 km"
+print("Ik heet",naam,"ik ben",leeftijd,"jaar.")
+print("Ik woon in",woonplaats,"samen met mijn moeder en mijn",huisdier,"met de naam Rosy.")
+print("Ik werk bij de",werk,"ongeveer 12 uur per week.")
+print("Ik zit op het",school,"ik doe de opleiding",opleiding,"en wil later Media developer worden.")
+print("De afstand van mijn huis naar school is",afstand,"maar gellukig ben ik in een halfuur op school.\n")
+print("Dit was de opdracht family ties.")
+
+
